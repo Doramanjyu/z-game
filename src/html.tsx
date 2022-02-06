4482ee6fd596549f0019794cc7342dd63153ffbf
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2022-02-06 20:33:31 +0900

    Tweak mobile view (#2)

---
+++ src/html.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+type Props = {
+  htmlAttributes: object
+  headComponents: Array<any>
+  bodyAttributes: object
+  preBodyComponents: Array<any>
+  body: string
+  postBodyComponents: Array<any>
+}
+
+export default function HTML(props: Props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=900, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
