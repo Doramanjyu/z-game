925d38cc5d15eabc0b2d98a0c50b405c00b5760a
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2022-02-09 23:55:15 +0900

    Simplify redundant member name

--- src/game/Kernel.ts
+++ src/game/Kernel.ts
@@ -12,27 +12,27 @@ type KernelCommand = {
 }
 
 export class Kernel {
-  readonly kernelAnime: {
+  readonly anime: {
     idle: Anime
     squat: Anime
     jump: Anime
   }
-  readonly kernelShadow: Splite
+  readonly shadow: Splite
 
   readonly ellasticCoeff: number
 
-  currentKernel: Anime
+  currentAnime: Anime
 
-  kernelPos!: Vec2
-  kernelVel!: Vec2
-  kernelJumpPow!: Vec2
-  kernelOnGround!: boolean
-  kernelDir!: number
+  pos!: Vec2
+  vel!: Vec2
+  jumpPow!: Vec2
+  onGround!: boolean
+  orientation!: number
 
   constructor(splite: HTMLImageElement) {
     this.ellasticCoeff = 0.5
 
-    this.kernelAnime = {
+    this.anime = {
       idle: new Anime(splite, {
         topLeft: [0, 0],
         sz: [12, 12],
@@ -49,8 +49,8 @@ export class Kernel {
         frames: [2, 2, 0, 2, 2, 2, 0],
       }),
     }
-    this.currentKernel = this.kernelAnime.idle
-    this.kernelShadow = new Splite(splite, {
+    this.currentAnime = this.anime.idle
+    this.shadow = new Splite(splite, {
       topLeft: [0, 24],
       sz: [12, 12],
     })
@@ -59,118 +59,118 @@ export class Kernel {
   }
 
   reset() {
-    this.kernelPos = [100, 96]
-    this.kernelVel = [0, 0]
-    this.kernelOnGround = true
-    this.kernelJumpPow = [0, 0]
-    this.kernelDir = 0
+    this.pos = [100, 96]
+    this.vel = [0, 0]
+    this.onGround = true
+    this.jumpPow = [0, 0]
+    this.orientation = 0
   }
 
   tick(cmd: KernelCommand, gameMap: GameMap<MapCell>) {
-    if (cmd.left && this.kernelOnGround) {
-      this.kernelJumpPow[0]--
-      if (this.kernelJumpPow[0] < -4) {
-        this.kernelJumpPow[0] = -4
+    if (cmd.left && this.onGround) {
+      this.jumpPow[0]--
+      if (this.jumpPow[0] < -4) {
+        this.jumpPow[0] = -4
       }
-      this.kernelDir = 0
+      this.orientation = 0
     }
-    if (cmd.right && this.kernelOnGround) {
-      this.kernelJumpPow[0]++
-      if (this.kernelJumpPow[0] > 4) {
-        this.kernelJumpPow[0] = 4
+    if (cmd.right && this.onGround) {
+      this.jumpPow[0]++
+      if (this.jumpPow[0] > 4) {
+        this.jumpPow[0] = 4
       }
-      this.kernelDir = 1
+      this.orientation = 1
     }
     if (
       !cmd.left &&
       !cmd.right &&
       !cmd.space &&
-      this.kernelOnGround &&
-      this.kernelJumpPow[0] !== 0 &&
-      this.kernelJumpPow[1] === 0
+      this.onGround &&
+      this.jumpPow[0] !== 0 &&
+      this.jumpPow[1] === 0
     ) {
-      this.kernelOnGround = false
-      this.kernelVel = [this.kernelJumpPow[0], -2]
-      this.kernelJumpPow = [0, 0]
+      this.onGround = false
+      this.vel = [this.jumpPow[0], -2]
+      this.jumpPow = [0, 0]
     }
 
-    this.kernelPos[0] += this.kernelVel[0]
-    this.kernelPos[1] += this.kernelVel[1]
-    const vBottom = this.kernelVel[1] > 0 ? this.kernelVel[1] : 0
+    this.pos[0] += this.vel[0]
+    this.pos[1] += this.vel[1]
+    const vBottom = this.vel[1] > 0 ? this.vel[1] : 0
     const mpBottom: Vec2 = [
-      Math.round((this.kernelPos[0] - 2) / 16),
-      Math.floor((this.kernelPos[1] + vBottom + 1) / 16),
+      Math.round((this.pos[0] - 2) / 16),
+      Math.floor((this.pos[1] + vBottom + 1) / 16),
     ]
-    const vUp = this.kernelVel[1] < 0 ? this.kernelVel[1] : 0
+    const vUp = this.vel[1] < 0 ? this.vel[1] : 0
     const mpUp: Vec2 = [
-      Math.round((this.kernelPos[0] + this.kernelVel[0] - 2) / 16),
-      Math.floor((this.kernelPos[1] + vUp - 4) / 16),
+      Math.round((this.pos[0] + this.vel[0] - 2) / 16),
+      Math.floor((this.pos[1] + vUp - 4) / 16),
     ]
     const mpSide: Vec2 = [
-      Math.round((this.kernelPos[0] + this.kernelVel[0] - 2) / 16),
-      Math.floor((this.kernelPos[1] - 1) / 16),
+      Math.round((this.pos[0] + this.vel[0] - 2) / 16),
+      Math.floor((this.pos[1] - 1) / 16),
     ]
 
-    this.currentKernel = this.kernelAnime.idle
-    if (cmd.space && this.kernelOnGround) {
-      this.currentKernel = this.kernelAnime.squat
-      this.kernelJumpPow[1] -= 2
-      if (this.kernelJumpPow[1] < -13) {
-        this.kernelJumpPow[1] = -13
+    this.currentAnime = this.anime.idle
+    if (cmd.space && this.onGround) {
+      this.currentAnime = this.anime.squat
+      this.jumpPow[1] -= 2
+      if (this.jumpPow[1] < -13) {
+        this.jumpPow[1] = -13
       }
     } else {
-      if (!cmd.space && this.kernelJumpPow[1] < 0) {
-        this.kernelOnGround = false
-        this.kernelVel = this.kernelJumpPow
-        this.kernelJumpPow = [0, 0]
+      if (!cmd.space && this.jumpPow[1] < 0) {
+        this.onGround = false
+        this.vel = this.jumpPow
+        this.jumpPow = [0, 0]
       }
-      if (!this.kernelOnGround) {
-        this.kernelVel[1] += 2
-        if (this.kernelVel[1] > 14) {
-          this.kernelVel[1] = 14
+      if (!this.onGround) {
+        this.vel[1] += 2
+        if (this.vel[1] > 14) {
+          this.vel[1] = 14
         }
-        if (gameMap.at(mpUp).solid && this.kernelVel[1] < 0) {
-          this.kernelVel[1] *= -this.ellasticCoeff
-          this.kernelVel[0] *= this.ellasticCoeff
-          this.kernelPos[1] = mpUp[1] * 16 + 16 + 4
+        if (gameMap.at(mpUp).solid && this.vel[1] < 0) {
+          this.vel[1] *= -this.ellasticCoeff
+          this.vel[0] *= this.ellasticCoeff
+          this.pos[1] = mpUp[1] * 16 + 16 + 4
         } else if (gameMap.at(mpSide).solid) {
-          this.kernelVel[0] *= -this.ellasticCoeff
+          this.vel[0] *= -this.ellasticCoeff
         }
-        if (gameMap.at(mpBottom).solid && this.kernelVel[1] >= 0) {
-          this.kernelPos[1] = mpBottom[1] * 16
-          this.kernelVel = [0, 0]
-          this.kernelOnGround = true
-          this.currentKernel = this.kernelAnime.squat
+        if (gameMap.at(mpBottom).solid && this.vel[1] >= 0) {
+          this.pos[1] = mpBottom[1] * 16
+          this.vel = [0, 0]
+          this.onGround = true
+          this.currentAnime = this.anime.squat
         }
-        if (this.kernelVel[1] < 0) {
-          this.currentKernel = this.kernelAnime.jump
-        } else if (this.kernelVel[1] > 0) {
-          this.currentKernel = this.kernelAnime.squat
+        if (this.vel[1] < 0) {
+          this.currentAnime = this.anime.jump
+        } else if (this.vel[1] > 0) {
+          this.currentAnime = this.anime.squat
         }
       }
     }
 
-    if (this.kernelPos[1] > 16 * 10) {
+    if (this.pos[1] > 16 * 10) {
       this.reset()
     }
   }
 
   draw(ctx: CanvasRenderingContext2D, scale: number) {
-    const kernelMode = this.currentKernel.tick()
-    if (this.kernelOnGround) {
-      this.kernelShadow.draw(
+    const kernelMode = this.currentAnime.tick()
+    if (this.onGround) {
+      this.shadow.draw(
         ctx,
-        [this.kernelPos[0], this.kernelPos[1] - 5],
+        [this.pos[0], this.pos[1] - 5],
         scale,
         kernelMode,
         0,
       )
     }
-    this.currentKernel.draw(
+    this.currentAnime.draw(
       ctx,
-      [this.kernelPos[0], this.kernelPos[1] - 6],
+      [this.pos[0], this.pos[1] - 6],
       scale,
-      this.kernelDir,
+      this.orientation,
       0,
     )
   }
