7d2fe8f59d47edfc5078a2f40c99111d9602e653
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2022-02-11 12:59:35 +0900

    Add map overlay animation

--- src/game/MapCell.ts
+++ src/game/MapCell.ts
@@ -5,11 +5,20 @@ export class MapCell implements Cell {
   readonly solid: boolean
 
   constructor(mode1: number, mode2: number, solid: boolean) {
-    this.v = {
-      mode1,
-      mode2,
-    }
+    this.v = { mode1, mode2 }
     this.solid = solid
+  }
+
+  appearance(): Appearance {
+    return this.v
+  }
+}
+
+export class OverlayMapCell implements Cell {
+  readonly v: Appearance
+
+  constructor(mode1: number, mode2: number) {
+    this.v = { mode1, mode2 }
   }
 
   appearance(): Appearance {
