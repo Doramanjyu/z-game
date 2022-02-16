bbe3bb5ac0b722428710bc27aaf40ebb1a18bf53
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2022-02-16 19:04:04 +0900

    Add aduh text (#80)

--- src/game/lib/Anime.ts
+++ src/game/lib/Anime.ts
@@ -4,12 +4,15 @@ import { Sprite, SpriteProp, Drawer } from './Sprite'
 export type AnimeProp = SpriteProp & {
   frames: number[]
   patterns?: number
+  countDiv?: number
 }
 
 export class Anime extends Sprite implements Drawer {
   readonly prop: AnimeProp
-  readonly patterns: number
-  cnt = 0
+  private readonly patterns: number
+  private readonly countDiv: number
+  private cnt = 0
+  frame = 0
 
   constructor(sprite: HTMLImageElement, prop: AnimeProp) {
     super(sprite, prop)
@@ -18,6 +21,11 @@ export class Anime extends Sprite implements Drawer {
       this.patterns = prop.patterns
     } else {
       this.patterns = prop.frames.reduce((acc, v) => (acc > v ? acc : v), 0)
+    }
+    if (prop.countDiv) {
+      this.countDiv = prop.countDiv
+    } else {
+      this.countDiv = 1
     }
   }
 
@@ -32,13 +40,16 @@ export class Anime extends Sprite implements Drawer {
       ctx,
       p,
       scale,
-      this.prop.frames[this.cnt] + this.patterns * mode2,
+      this.prop.frames[this.frame] + this.patterns * mode2,
       mode1,
     )
   }
 
   tick(): number {
-    this.cnt = (this.cnt + 1) % this.prop.frames.length
-    return this.prop.frames[this.cnt]
+    this.cnt++
+    this.frame = Math.floor(
+      (this.cnt / this.countDiv) % this.prop.frames.length,
+    )
+    return this.prop.frames[this.frame]
   }
 }
