56c51793b0c87312abd4b467b6f03a6f75c3a53e
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2022-02-23 22:45:27 +0900

    Add dialog manager class

---
+++ src/game/dialog.ts
@@ -0,0 +1,30 @@
+type DialogOption = {
+  timeout?: number
+}
+
+export class DialogManager {
+  readonly messageBox: HTMLDivElement
+
+  private timeoutTimer?: ReturnType<typeof setTimeout>
+
+  constructor(messageBox: HTMLDivElement) {
+    this.messageBox = messageBox
+  }
+
+  showMessage(text: string, opt: DialogOption) {
+    if (this.timeoutTimer) {
+      clearTimeout(this.timeoutTimer)
+    }
+    this.messageBox.innerText = text
+    this.messageBox.style.display = 'block'
+    this.messageBox.classList.remove('hide')
+
+    if (opt.timeout) {
+      this.timeoutTimer = setTimeout(this.hideMessage.bind(this), opt.timeout)
+    }
+  }
+
+  hideMessage() {
+    this.messageBox.classList.add('hide')
+  }
+}
