8b2bb548565ac18ce8b7872e2291dfb5a0a6063f
Doramanjyu <24859285+Doramanjyu@users.noreply.github.com>
2022-02-26 23:36:02 +0900

    Add scale to map editor

--- src/game/MapEditor.tsx
+++ src/game/MapEditor.tsx
@@ -22,6 +22,7 @@ const MapEditor: React.FC<Props> = ({ sprite }) => {
   )
   const [cursor, setCursor] = useState<Vec2>([0, 0])
   const [value, setValue] = useState<Vec2>([0, 0])
+  const [scale, setScale] = useState(2)
   const [_, setVersion] = useState(0)
   const [layer, setLayer] = useState('main')
   const updateValue = (d: Vec2) => {
@@ -45,131 +46,167 @@ const MapEditor: React.FC<Props> = ({ sprite }) => {
       style={{
         width: '100%',
         height: '100vh',
-        overflow: 'scroll',
       }}
-      css={css`
-        div.row {
-          margin: 0;
-          padding: 0;
-          height: 16px;
-          white-space: nowrap;
-        }
-        div.row > div {
-          width: 16px;
-          height: 16px;
-          position: relative;
-          left: 0;
-          top: 0;
-          display: inline-block;
-        }
-        div.row > div > span {
-          display: block;
-          background-image: url(${sprite});
-          image-rendering: pixelated;
-          position: absolute;
-          left: 0;
-          top: 0;
-          width: 16px;
-          height: 16px;
-        }
-      `}
     >
-      {[...Array(gameMap.sz[1])].map((_, j) => (
-        <div key={`row${j}`} className="row">
-          {[...Array(gameMap.sz[0])].map((_, i) => {
-            const cell = gameMap.at([i, j])
-            const main = cell.appearance('main')
-            const under = cell.appearance('under')
-            const overlay = cell.appearance('overlay')
-            const overlayAnime = cell.appearance('overlayAnime')
-            const [mx, my] = [main[0] * 16, 512 + main[1] * 16]
-            const [ux, uy] = [512 + under[0] * 16, 512 + under[1] * 16]
-            const [ox, oy] = [overlay[0] * 16, 896 + overlay[1] * 16]
-            const [oax, oay] = [
-              overlayAnime[0] * 16,
-              768 + overlayAnime[1] * 16,
-            ]
-            return (
-              <div key={`col${i}`} onClick={() => updateCursor([i, j])}>
-                <span
-                  style={{
-                    backgroundPosition: `-${ux * 2}px -${uy * 2}px`,
-                    backgroundSize: `2048px 2048px`,
-                    width: '32px',
-                    height: '32px',
-                  }}
-                />
-                <span
-                  style={{
-                    backgroundPosition: `-${mx}px -${my}px`,
-                  }}
-                />
-                <span
-                  style={{
-                    backgroundPosition: `-${ox}px -${oy}px`,
-                  }}
-                />
-                <span
-                  style={{
-                    backgroundPosition: `-${oax}px -${oay}px`,
-                  }}
-                />
-                {cursor[0] === i && cursor[1] === j && (
+      <div
+        style={{
+          width: '100%',
+          height: '2rem',
+          backgroundColor: '#333',
+          display: 'flex',
+          padding: '4px 16px',
+          boxSizing: 'border-box',
+          alignItems: 'center',
+          color: 'white',
+          fontSize: '12px',
+        }}
+      >
+        <label htmlFor="scale">scale</label>
+        <select
+          id="scale"
+          defaultValue={2}
+          onChange={(e) => setScale(parseInt(e.target.value))}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          height: 'calc(100vh - 2rem)',
+          overflow: 'scroll',
+        }}
+        css={css`
+          div.row {
+            margin: 0;
+            padding: 0;
+            height: ${16 * scale}px;
+            white-space: nowrap;
+          }
+          div.row > div {
+            width: ${16 * scale}px;
+            height: ${16 * scale}px;
+            position: relative;
+            left: 0;
+            top: 0;
+            display: inline-block;
+          }
+          div.row > div > span {
+            display: block;
+            background-image: url(${sprite});
+            background-size: ${1024 * scale}px ${1024 * scale}px;
+            image-rendering: pixelated;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: ${16 * scale}px;
+            height: ${16 * scale}px;
+          }
+        `}
+      >
+        {[...Array(gameMap.sz[1])].map((_, j) => (
+          <div key={`row${j}`} className="row">
+            {[...Array(gameMap.sz[0])].map((_, i) => {
+              const cell = gameMap.at([i, j])
+              const main = cell.appearance('main')
+              const under = cell.appearance('under')
+              const overlay = cell.appearance('overlay')
+              const overlayAnime = cell.appearance('overlayAnime')
+              const [mx, my] = [main[0] * 16, 512 + main[1] * 16]
+              const [ux, uy] = [512 + under[0] * 16, 512 + under[1] * 16]
+              const [ox, oy] = [overlay[0] * 16, 896 + overlay[1] * 16]
+              const [oax, oay] = [
+                overlayAnime[0] * 16,
+                768 + overlayAnime[1] * 16,
+              ]
+              return (
+                <div key={`col${i}`} onClick={() => updateCursor([i, j])}>
                   <span
                     style={{
-                      backgroundPosition: `-992px 0`,
+                      backgroundPosition: `-${ux * 2 * scale}px -${
+                        uy * 2 * scale
+                      }px`,
+                      backgroundSize: `${2048 * scale}px ${2048 * scale}px`,
+                      width: `${32 * scale}px`,
+                      height: `${32 * scale}px`,
                     }}
                   />
-                )}
-              </div>
-            )
-          })}
-        </div>
-      ))}
-      <Draggable>
-        <div
-          style={{
-            backgroundColor: 'white',
-            opacity: 0.7,
-            width: '196px',
-            height: 'auto',
-            padding: '4px',
-            fontSize: '10px',
-            color: 'black',
-            borderRadius: '2px',
-            borderTop: '10px solid #999',
-            textAlign: 'center',
-            position: 'fixed',
-            bottom: '16px',
-            right: '16px',
-          }}
-          css={css`
-            button,
-            select {
-              font-size: 10px;
-              margin: 1px 0;
-              font-family: inherit;
-            }
-          `}
-        >
-          <div>
-            <button onClick={() => updateValue([-1, 0])}>&lt;</button>{' '}
-            {value[0]} <button onClick={() => updateValue([1, 0])}>&gt;</button>{' '}
-            <button onClick={() => updateValue([0, -1])}>&lt;</button>{' '}
-            {value[1]} <button onClick={() => updateValue([0, 1])}>&gt;</button>
+                  <span
+                    style={{
+                      backgroundPosition: `-${mx * scale}px -${my * scale}px`,
+                    }}
+                  />
+                  <span
+                    style={{
+                      backgroundPosition: `-${ox * scale}px -${oy * scale}px`,
+                    }}
+                  />
+                  <span
+                    style={{
+                      backgroundPosition: `-${oax * scale}px -${oay * scale}px`,
+                    }}
+                  />
+                  {cursor[0] === i && cursor[1] === j && (
+                    <span
+                      style={{
+                        backgroundPosition: `-${992 * scale}px 0`,
+                      }}
+                    />
+                  )}
+                </div>
+              )
+            })}
           </div>
-          <label htmlFor="layerSelect">layer</label>
-          <select
-            onChange={(e) => changeLayer(e.target.value)}
-            defaultValue="main"
+        ))}
+        <Draggable>
+          <div
+            style={{
+              backgroundColor: 'white',
+              opacity: 0.7,
+              width: '200px',
+              height: 'auto',
+              padding: '4px',
+              fontSize: '14px',
+              color: 'black',
+              borderRadius: '2px',
+              borderTop: '1em solid #999',
+              textAlign: 'center',
+              position: 'fixed',
+              bottom: '32px',
+              right: '32px',
+            }}
+            css={css`
+              button,
+              select {
+                font-size: inherit;
+                margin: 2px 0;
+              }
+            `}
           >
-            <option value="main">main</option>
-            <option value="under">under</option>
-            <option value="overlay">overlay</option>
-            <option value="overlayAnime">overlay anime</option>
-          </select>
-        </div>
-      </Draggable>
+            <div>
+              <button onClick={() => updateValue([-1, 0])}>&lt;</button>{' '}
+              {value[0]}{' '}
+              <button onClick={() => updateValue([1, 0])}>&gt;</button>{' '}
+              <button onClick={() => updateValue([0, -1])}>&lt;</button>{' '}
+              {value[1]}{' '}
+              <button onClick={() => updateValue([0, 1])}>&gt;</button>
+            </div>
+            <label htmlFor="layerSelect">layer</label>
+            <select
+              id="layerSelect"
+              onChange={(e) => changeLayer(e.target.value)}
+              defaultValue="main"
+            >
+              <option value="main">main</option>
+              <option value="under">under</option>
+              <option value="overlay">overlay</option>
+              <option value="overlayAnime">overlay anime</option>
+            </select>
+          </div>
+        </Draggable>
+      </div>
     </div>
   )
 }
